f75d24b4cf3a0947594830346912a03b9f1b413a
andy-mf <88088166+andy-mf@users.noreply.github.com>
2026-03-19 11:48:54 +0000

    Added chess pieces and started gamelist

--- src/Routing.kt
+++ src/Routing.kt
@@ -2,12 +2,17 @@ package com.example
 
 import com.example.games.Deck
 import io.ktor.server.application.*
+import io.ktor.server.http.content.staticFiles
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.pebble.respondTemplate
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+//import java.io.File
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/resources", "/static")
+
         get("/") {
             //call.respondText("Hello World!!!!")
             call.respondTemplate("login.peb", model=mapOf("title" to "Login"))
@@ -16,6 +21,11 @@ fun Application.configureRouting() {
         get("/signup")
         {
             call.respondTemplate("signup.peb", model=mapOf("title" to "Sign up"))
+        }
+
+        get("/gamelist")
+        {
+            call.respondTemplate("gamelist.peb", model=emptyMap())
         }
     }
 }
