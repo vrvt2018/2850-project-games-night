f263e1318ca55389ebbb65db08e48e7d0687ae54
andy-mf <88088166+andy-mf@users.noreply.github.com>
2026-03-19 14:46:00 +0000

    Started construction of game list

--- src/Routing.kt
+++ src/Routing.kt
@@ -2,12 +2,25 @@ package com.example
 
 import com.example.games.Deck
 import io.ktor.server.application.*
+import io.ktor.server.http.content.resources
 import io.ktor.server.http.content.staticFiles
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.pebble.respondTemplate
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 //import java.io.File
+
+class GameEntity {
+    val name: String
+    val imgUrl: String
+
+    constructor(name: String, imgUrl: String) {
+        this.name = name
+        this.imgUrl = imgUrl
+    }
+
+    fun getAltText() = name + " logo"
+}
 
 fun Application.configureRouting() {
     routing {
@@ -26,6 +39,22 @@ fun Application.configureRouting() {
         get("/gamelist")
         {
             call.respondTemplate("gamelist.peb", model=emptyMap())
+        }
+
+        get("/game")
+        {
+
+            /*
+            val games = arrayOf( {
+                GameEntity("Chess", "resources/assets/chess/Chess_Clt45.svg")
+                GameEntity("Go Fish", "resources/static/assets/Chess_Clt45.svg")
+            })
+
+            val gamesList = arrayOf( {
+                "<li><img src=\" alt=\"\"></img></li>"
+            })*/
+
+            call.respondTemplate("game.peb", model=emptyMap())//model=mapOf(gamesList to gamesList))
         }
     }
 }
